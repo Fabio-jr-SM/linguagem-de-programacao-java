49e5ea4adfdf46832b1078b7ce24e7b0d081d0b9
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2022-11-28 14:30:05 -0400

    Create pessoa.java

---
+++ Lp-2/pessoa.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private Date data_nascimento;
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+  
+  public void setCpf(String cpf) {
+		this.nome = cpf;
+	}
+  
+  public void setDataNascimento(String data_nascimento) {
+		this.nome = data_nascimento;
+	}
+}
