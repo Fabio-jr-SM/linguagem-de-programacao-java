af4a3aba4ae0927c7e92164ea4b49054f9a6469b
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2022-11-28 15:01:57 -0400

    Update main.java

--- Lp-2/main.java
+++ Lp-2/main.java
@@ -1,7 +1,20 @@
 public class main {
 	public static void main(String[] args) {
-		Aluno i = new Aluno("cafu ronaldo", "123.987.129-00", new Date());
+		Aluno i = new aluno("cafu ronaldo", "123.987.129-00", new Date());
+		System.out.println("Veja como os atributos foram preenchidos\n\nNome: " + i.nome);
+		System.out.println("CPF: " + i.cpf);
+		System.out.println("Data de nascimento: " + i.data_nascimento.toString());
+
+
+                Professor i = new professor ("cafu ronaldo", "123.987.129-00", new Date());
+		System.out.println("Veja como os atributos foram preenchidos\n\nNome: " + i.nome);
+		System.out.println("CPF: " + i.cpf);
+		System.out.println("Data de nascimento: " + i.data_nascimento.toString());
+
+
+                Funcionario i = new funcionario("cafu ronaldo", "123.987.129-00", new Date());
 		System.out.println("Veja como os atributos foram preenchidos\n\nNome: " + i.nome);
 		System.out.println("CPF: " + i.cpf);
 		System.out.println("Data de nascimento: " + i.data_nascimento.toString());
 	}
+}
