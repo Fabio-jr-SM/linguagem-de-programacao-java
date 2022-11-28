d4906bb665497699e7f9d3b8cc6d8f00de887b14
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2022-11-28 14:46:28 -0400

    Update pessoa.java

--- Lp-2/pessoa.java
+++ Lp-2/pessoa.java
@@ -3,16 +3,28 @@ public class Pessoa {
 	private String nome;
 	private String cpf;
 	private Date data_nascimento;
-
+	
+	public String getNome(){
+		return this.nome;
+	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-  
-  public void setCpf(String cpf) {
+	
+	public String getCpf(){
+		return this.cpf;
+	}
+	
+	public void setCpf(String cpf) {
 		this.nome = cpf;
 	}
-  
-  public void setDataNascimento(String data_nascimento) {
+	
+	public Date getDataNascimento(){
+		return this.data_nascimento;
+	}
+
+	public void setDataNascimento(String data_nascimento) {
 		this.nome = data_nascimento;
 	}
 }
